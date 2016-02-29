66fc53cb19a6a2b4443500d45f1b14510e32365e
Cedric Rolland <c.rolland88@gmail.com>
2016-02-29 11:31:43 +0100

    modified url

--- hello/views.py
+++ hello/views.py
@@ -18,6 +18,7 @@ def db(request):
 
     return render(request, 'db.html', {'greetings': greetings})
 
-
-def list_wines():
-	return HttpResponse('yeay, my first call <3')
+def list_wines(requests):
+	r = requests.get('http://httpbin.org/status/418')
+    print r.text
+    return HttpResponse('<pre>' + r.text + '</pre>')
