9a651958c448b0d5739cc8ea49c6114b17777f53
Cedric Rolland <c.rolland88@gmail.com>
2016-03-11 17:54:52 +0100

    fea2

--- intelliwineApp/admin.py
+++ intelliwineApp/admin.py
@@ -3,7 +3,7 @@ from django.contrib import admin
 from django import forms
 from django.contrib.auth.forms import UserCreationForm, UserChangeForm
 from django.contrib.auth.admin import UserAdmin as AuthUserAdmin
-from .models import User
+from django.contrib.auth.models import User
 
 
 class MyUserCreationForm(UserCreationForm):
